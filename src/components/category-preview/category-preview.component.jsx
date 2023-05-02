c48d68e4208a95f46762e1225dc22ca6944bacc3
GuoXingqi <guo_xingqi@hotmail.com>
2023-05-02 01:20:00 -0400

    Finished redux-thunk spinner feature on category and categories-preview

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,32 +1,23 @@
 import ProductCard from '../product-card/product-card.component';
-import Spinner from '../spinner/spinner.component';
-
-import { useSelector } from 'react-redux';
-import { selectCategoriesIsLoading } from '../../store/categories/categories.selector';
 
 import { CategoryPreviewContainer, Title, Preview } from './category-preview.styles';
 
 const CategoryPreview = ({ title, products })=>{
-
-  const categoriesIsLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <CategoryPreviewContainer>
       <h2>
         <Title to={title}>{title.toUpperCase()}</Title>
       </h2>
-      {
-        categoriesIsLoading ? <Spinner /> :
-          <Preview>
-            {
-              products
-                .filter((_, idx) => idx < 4 )
-                .map( ( product ) => (
-                  <ProductCard key={product.id} product={product} />
-                ))
-            }
-          </Preview>
-      }
+      <Preview>
+        {
+          products
+            .filter((_, idx) => idx < 4 )
+            .map( ( product ) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+        }
+      </Preview>
     </CategoryPreviewContainer>
   )
 }
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,22 +1,28 @@
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import { selectCategoriesIsLoading, selectCategoriesMap } from "../../store/categories/categories.selector";
 
+import { Fragment } from "react";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Spinner from '../../components/spinner/spinner.component';
 
 const CategoriesPreview = () => {
 
   const categoriesMap = useSelector(selectCategoriesMap);
+  const categoriesIsLoading = useSelector(selectCategoriesIsLoading);
 
   return (
+    <Fragment>
 
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title)=>{//object.keys returns an array of keys
-        const products = categoriesMap[title];
-        return(
-        <CategoryPreview key={title} title={title} products={products}></CategoryPreview>
-        );
-      })}
-    </div>
+      {
+        categoriesIsLoading ? <Spinner /> :
+          (Object.keys(categoriesMap).map((title)=>{//object.keys returns an array of keys
+            const products = categoriesMap[title];
+            return(
+            <CategoryPreview key={title} title={title} products={products}></CategoryPreview>
+            );
+          }))
+      }
+    </Fragment>
   );
 }
 
